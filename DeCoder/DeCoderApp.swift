18473c6f123ea8e201fe19fd60b7fb0c4cd8d303
Chrissshere <christiantashev@gmail.com>
2025-03-29 23:54:02 +0200

    Initial Commit

---
+++ DeCoder/DeCoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeCoderApp.swift
+//  DeCoder
+//
+//  Created by Christian Tashev on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DeCoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeCoderTests/DeCoderTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeCoderTests.swift
+//  DeCoderTests
+//
+//  Created by Christian Tashev on 3/29/25.
+//
+
+import Testing
+@testable import DeCoder
+
+struct DeCoderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
